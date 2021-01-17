e32747d69bba4b8625bf2af135ef40f7674ea072
rhridwan <ridwanul.haque@northsouth.edu>
2021-01-17 19:50:04 +0600

    added

---
+++ FirstSL/main.c
@@ -0,0 +1,19 @@
+
+int Add(int i1, int i2)
+{
+    return i1 + i2;
+}
+long long Mul(int i1, int i2)
+{
+    return i1 * i2;
+}
+int Sub(int i1, int i2)
+{
+    return i1 - i2;
+}
+int Div(int i1, int i2)
+{
+    if(i2)
+    return i1 / i2;
+    return -1;
+}
---
+++ FirstUsingSL/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Add(int,int);
+long long Mul(int,int);
+int Sub(int,int);
+int Div(int,int);
+
+int main()
+{
+    printf( "Hello World\n %d\n",Add(4,4) );
+    return 0;
+}
